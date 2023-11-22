9b2df70496d9ff3a900110622e4614324c9520a7
devleejb <devleejb@gmail.com>
2023-11-23 00:43:15 +0900

    feat: Automate test code running

--- project-2/2018008977/2_Parser/test.sh
+++ project-2/2018008977/2_Parser/test.sh
@@ -1,8 +1,6 @@
-./cminus_parser ./example/test.1.txt > ./example/my_result.1.txt
-./cminus_parser ./example/test.2.txt > ./example/my_result.2.txt
-
-echo "CMinus Parser Test 1"
-diff ./example/my_result.1.txt ./example/result.1.txt
-
-echo "CMinus Parser Test 2"
-diff ./example/my_result.2.txt ./example/result.2.txt
+for i in {1..4}
+do
+    ./cminus_parser ./example/test.$i.txt > ./example/my_result.$i.txt
+    echo "CMinus Parser Test $i"
+    diff ./example/my_result.$i.txt ./example/result.$i.txt
+done
