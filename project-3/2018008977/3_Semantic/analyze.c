744edbdaa9d538c429fab76247f0b81c119e95dd
devleejb <devleejb@gmail.com>
2023-12-04 18:05:32 +0900

    feat: Implement analyzer for `VarAccessExpr`

--- project-3/2018008977/3_Semantic/analyze.c
+++ project-3/2018008977/3_Semantic/analyze.c
@@ -220,14 +220,11 @@ static void insertNode(TreeNode *t)
 		case VarAccessExpr:
 		{
 			// Semantic Error: Undeclared Variables
+			SymbolRec *func = lookupSymbolWithKind(globalScope, t->name, VariableSym);
+			if (func == NULL) func = UndeclaredVariableError(globalScope, t);
 			// Update Symbol Table Entry
-			/*********************Fill the Code*************************
-			 *                                                         *
-			 *                                                         *
-			 *                                                         *
-			 *                                                         *
-			 *                                                         *
-			************************************************************/
+			else
+				appendSymbol(globalScope, t->name, t->lineno);
 			
 			// Break
 			break;
